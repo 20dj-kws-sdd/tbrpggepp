5863d56fc34003bbe0092a4a010f50c4c5f5aa98
Quiterion <quiterion12@gmail.com>
2020-06-03 12:30:53 +1000

    Build intro and main menu forms with python files for functionality

---
+++ introForm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sys
+import time
+from pathlib import Path
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+class mainForm(QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        uic.loadUi("mainForm.ui", self)
+        self.title = "tbrpggepp"
+
+    def showDialog(self):
+        # from "http://zetcode.com/gui/pyqt5/dialogs/"
+        home_dir = str(Path.home())  # Cross-platform technique for finding home directory
+        fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
+
+        if fname[0]:
+            f = open(fname[0], 'r')
+            with f:
+                data = f.read()
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = mainForm()
+    ex.show()
+    sys.exit(app.exec_())
