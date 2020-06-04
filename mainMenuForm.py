c3c4539cc491262deeadba7af3eaf1ff5b11709d
Quiterion <quiterion12@gmail.com>
2020-06-04 11:43:17 +1000

    Finish building UI for main menu form

--- mainMenuForm.py
+++ mainMenuForm.py
@@ -12,20 +12,8 @@ class mainForm(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        uic.loadUi("introForm.ui", self)
+        uic.loadUi("mainMenuForm.ui", self)
         self.title = "tbrpggepp"
-
-    def showDialog(self):
-        # from "http://zetcode.com/gui/pyqt5/dialogs/"
-        home_dir = str(Path.home())  # Cross-platform technique for finding home directory
-        fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
-
-        if fname[0]:
-            f = open(fname[0], 'r')
-            with f:
-                data = f.read()
-
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
