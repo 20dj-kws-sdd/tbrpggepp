b482988effd3f8b0cf5445b197df9c5a67ebfe79
Quiterion <quiterion12@gmail.com>
2020-06-18 11:33:22 +1000

    Really finish building UI forms

--- editTileForm.py
+++ editTileForm.py
@@ -8,7 +8,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-class mainForm(QMainWindow):
+class editTileForm(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -17,6 +17,6 @@ class mainForm(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = mainForm()
+    ex = editTileForm()
     ex.show()
     sys.exit(app.exec_())
--- mainMenuForm.py
+++ mainMenuForm.py
@@ -2,21 +2,28 @@
 
 import sys
 import time
+import json
 from pathlib import Path
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-class mainForm(QMainWindow):
+class mainMenuForm(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         uic.loadUi("UI_Layouts/mainMenuForm.ui", self)
         self.title = "tbrpggepp"
+        self.world_file = None
+        self.game_world = {}
+
+    def loadWorldFile(self, world_file_path):
+        self.world_file = open(word_file_path, 'rw')
+        self.game
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = mainForm()
+    ex = mainMenuForm()
     ex.show()
     sys.exit(app.exec_())
