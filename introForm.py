fd4d6f92b6fd56394fd3421cc1c18232d54212b9
Quiterion <quiterion12@gmail.com>
2020-06-18 11:39:59 +1000

    Add notes

--- introForm.py
+++ introForm.py
@@ -7,28 +7,42 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from mainMenuForm import *
 
-class mainForm(QMainWindow):
+class introForm(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
         uic.loadUi("UI_Layouts/introForm.ui", self)
         self.title = "tbrpggepp"
 
-    def showDialog(self):
+        # Create new object for next form
+        self.mainMenu = mainMenuForm()
+
+        # Add connections
+        self.btnOpenFile.clicked.connect(self.btnOpenFileClicked)
+        self.btnNewWorld.clicked.connect(self.btnNewWorldClicked)
+
+    def btnOpenFileClicked(self):
         # from "http://zetcode.com/gui/pyqt5/dialogs/"
         home_dir = str(Path.home())  # Cross-platform technique for finding home directory
-        fname = QFileDialog.getOpenFileName(self, 'Open file', home_dir)
+        fname = QFileDialog.getOpenFileName(self, 'Open world file', home_dir)
 
         if fname[0]:
-            f = open(fname[0], 'r')
-            with f:
-                data = f.read()
+            if not fname[0].endswith(".json"):
+                error_dialog = QErrorMessage(self)
+                error_dialog.setWindowTitle("Invalid file")
+                error_dialog.showMessage('Invalid world map file!')
+            else:
+                self.mainMenu.show()
+                self.hide()
 
-
+    def btnNewWorldClicked(self):
+        self.mainMenu.show()
+        self.hide()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = mainForm()
+    ex = introForm()
     ex.show()
     sys.exit(app.exec_())
